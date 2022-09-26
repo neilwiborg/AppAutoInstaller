3f25908cd0517b56db97ad3c6fbc62da90bb7ebd
Neil Wiborg <NeilWiborg@outlook.com>
2022-09-26 00:34:22 -0700

    add scrollbar to itemlist and add package manager selection

--- src/itemList.py
+++ src/itemList.py
@@ -1,14 +1,37 @@
+from dataclasses import dataclass
 from tkinter import *
 from tkinter import ttk
 
 from typing import List
 
-class ItemList:
-	def __init__(self, root, items: List[str]) -> None:
-		self.frame = ttk.Frame(root)
-		for item in items:
-			listItem = ttk.Checkbutton(self.frame, text=item)
-			listItem.pack(side=TOP)
+@dataclass
+class ListItem:
+	box: ttk.Checkbutton
+	checked: IntVar
 
-	def pack(self) -> None:
-		self.frame.pack()
+class ItemList(ttk.Frame):
+	def __init__(self, root: Tk, names: List[str], *args, **kw) -> None:
+		super().__init__(root, *args, **kw)
+		canvas = Canvas(self, highlightthickness=0)
+		scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
+		self.frame = ttk.Frame(canvas)
+		self.items: List[ListItem] = []
+		
+		self.frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+		canvas.create_window((0, 0), window=self.frame, anchor="nw")
+		canvas.configure(yscrollcommand=scrollbar.set)
+		canvas.pack(side="left", fill="both", expand=True)
+		scrollbar.pack(side="right", fill="y")
+		
+		for name in names:
+			check: IntVar = IntVar()
+			check.set(1)
+			listItem: ListItem = ListItem(ttk.Checkbutton(self.frame, text=name, variable=check), check)
+			listItem.box.grid(row=len(self.items),column=0, sticky="w")
+			self.items.append(listItem)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,53 @@
+from struct import pack
 from tkinter import *
 from tkinter import ttk
 
 from itemList import ItemList
 
 class AppAutoInstaller:
-	def __init__(self, root) -> None:
+	def __init__(self, root: Tk) -> None:
 		root.title("App Auto Installer")
 		root.minsize(700, 700)
-		myList = ItemList(root, ["s1", "s2", "s3"])
-		myList.pack()
+		root.option_add('*tearOff', False)
 
+		menubar = Menu(root)
+		root.config(menu = menubar)
+		file = Menu(menubar)
+		edit = Menu(menubar)
+		help_ = Menu(menubar)
+		menubar.add_cascade(menu = file, label = "File")
+		menubar.add_cascade(menu = edit, label = "Edit")
+		menubar.add_cascade(menu = help_, label = "Help")
+		file.add_command(label = "Import")
+		file.add_command(label = "Export")
+
+		itemsFrame = ttk.Frame(root)
+		ttk.Label(itemsFrame, text="Programs to install:").grid(row=0,column=0, sticky="nw")
+		myList = ItemList(itemsFrame, ["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13", "s14", "s15", "s16", "s17", "s18", "s19", "s20", "s21", "s22", "s23", "s24"])
+		myList.grid(row=1, column=0)
+		itemsFrame.grid(row=0,column=0, sticky="nw")
+
+		packageManagerFrame = ttk.Frame(root)
+		ttk.Label(packageManagerFrame, text="Package manager:").grid(row=0, column=0, sticky="nw")
+		packageManagerOptions = [
+			"winget",
+			"scoop",
+			"apt-get"
+		]
+		dropdownSelection = StringVar()
+		dropdownSelection.set(packageManagerOptions[0])
+		dropdown = ttk.OptionMenu(packageManagerFrame, dropdownSelection, *packageManagerOptions)
+		dropdown.grid(row=1, column=0, sticky="nw")
+		installButton = ttk.Button(packageManagerFrame, text="Install selected packages")
+		installButton.grid(row=2, column=0, sticky="nw")
+		packageManagerFrame.grid(row=0, column=1, sticky="nw")
+		
+		itemButtonsFrame = ttk.Frame(root)
+		importButton = ttk.Button(itemButtonsFrame, text="Import")
+		exportButton = ttk.Button(itemButtonsFrame, text="Export")
+		importButton.grid(row=0, column=0)
+		exportButton.grid(row=0, column=1)
+		itemButtonsFrame.grid(row=1, column=0, sticky="nw")
 
 def main() -> None:
 	root = Tk()
